8f806e8721aa043b4c13600272d6314a5625fdec
Whelan Callahan <callahanwhelan@gmail.com>
2026-03-14 11:52:48 -0700

    fix startup command

--- programs/inventory-coordinator/0.1.0/startup.lua
+++ programs/inventory-coordinator/0.1.0/startup.lua
@@ -1,5 +1,15 @@
+local function programDir()
+  if shell and shell.getRunningProgram then
+    return fs.getDir(shell.getRunningProgram())
+  end
+
+  return ""
+end
+
+local entrypoint = fs.combine(programDir(), "src/coordinator.lua")
+
 if shell and shell.run then
-  shell.run("src/coordinator.lua")
+  shell.run(entrypoint)
 else
-  dofile("src/coordinator.lua")
+  dofile(entrypoint)
 end
