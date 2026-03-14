c76a6228b309235ea0419c1749f7a4fbc71ab48e
Whelan Callahan <callahanwhelan@gmail.com>
2026-03-14 11:56:02 -0700

    Update startup.lua

--- programs/wh-controller/0.1.0/startup.lua
+++ programs/wh-controller/0.1.0/startup.lua
@@ -1,1 +1,10 @@
-require("src.app.controller").run()
+local function programDir()
+  if shell and shell.getRunningProgram then
+    return fs.getDir(shell.getRunningProgram())
+  end
+
+  return ""
+end
+
+local controller = dofile(fs.combine(programDir(), "src/app/controller.lua"))
+controller.run()
