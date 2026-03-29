49c97c84f65cc796d5882c3afcc55618c6dbbae5
Whelan Callahan <callahanwhelan@gmail.com>
2026-03-29 13:26:49 -0700

    luaunit manifest wrapper

---
+++ lib/luaunit/manifest.lua
@@ -0,0 +1,12 @@
+return {
+  manifest_version = 1,
+  type = "library",
+  name = "luaunit",
+  version = "3.4",
+  source_base = "https://raw.githubusercontent.com/bluebird75/luaunit/LUAUNIT_V3_4",
+  source_prefix = nil,
+  files = {
+    "luaunit.lua",
+  },
+  deps = {},
+}
